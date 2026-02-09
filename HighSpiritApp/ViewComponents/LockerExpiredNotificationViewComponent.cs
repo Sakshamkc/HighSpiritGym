33101343db7de897b28cffc4799c40c5ad2551a2
saksham.kc <saksham.kc@cotiviti.com>
2026-02-09 15:50:49 +0000

    Added Expired notification based on gender

--- HighSpiritApp/ViewComponents/LockerExpiredNotificationViewComponent.cs
+++ HighSpiritApp/ViewComponents/LockerExpiredNotificationViewComponent.cs
@@ -15,11 +15,18 @@ namespace HighSpiritApp.ViewComponents
             _lockerService = lockerService;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(string? gender = null)
         {
             var expiredLockers = (await _lockerService.GetExpiredLockersAsync()).ToList();
 
+            // Filter by gender if specified
+            if (!string.IsNullOrEmpty(gender))
+            {
+                expiredLockers = expiredLockers.Where(l => l.Gender == gender).ToList();
+            }
+
             ViewBag.ExpiredCount = expiredLockers.Count;
+            ViewBag.CurrentGender = gender;
 
             return View(expiredLockers.Take(5).ToList());
         }
